47123a11d26b4f460accdb952c00fa75e6a39c48
mgore2003 <142228045+mgore2003@users.noreply.github.com>
2024-04-09 08:29:48 -0400

    Add files via upload

---
+++ project05-02.js
@@ -0,0 +1,22 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Project 05-02
+
+      Project to move images between a photo bucket and photo list.
+      Author: 
+      Date:   
+
+      Filename: project05-02.js
+*/
+
+let images = document.getElementsByTagName("img");
+let photoBucket = document.getElementById("photo_bucket");
+let photoList = document.getElementById("photo_list");
+
+for (let i = 0; i < images.length; i++) {
+	image.onclick = function(){
+		
+	}
+}
+
